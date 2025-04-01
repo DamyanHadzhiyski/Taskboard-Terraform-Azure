0630ec4cdb7668d7200758ccbb04012a494adcba
Damyan Hadzhiyski <damyan.hadzhiyski@gmail.com>
2025-04-01 21:46:46 +0300

    TaskBoard-Terraform-Actions

---
+++ main.tf
@@ -0,0 +1,79 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "4.25.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+  subscription_id = "55279d49-fe5a-40eb-93ae-034299220879"
+}
+
+resource "random_integer" "ri" {
+  min = 10000
+  max = 99999
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "${var.resource_group_name}-${random_integer.ri.result}"
+  location = var.resource_group_location
+}
+
+resource "azurerm_mssql_server" "sqlserver" {
+  name = "${var.sql_server_name}-${random_integer.ri.result}"
+  resource_group_name = azurerm_resource_group.rg.name
+  location = azurerm_resource_group.rg.location
+  administrator_login = var.sql_admin_login
+  administrator_login_password = var.sql_admin_password
+  version = "12.0"
+}
+
+resource "azurerm_mssql_database" "db" {
+  name = "${var.sql_database_name}-${random_integer.ri.result}"
+  server_id = azurerm_mssql_server.sqlserver.id  
+}
+
+resource "azurerm_mssql_firewall_rule" "sqlfirewall" {
+  name = "${var.firewall_rule_name}-${random_integer.ri.result}"
+  server_id = azurerm_mssql_server.sqlserver.id
+  start_ip_address = "0.0.0.0"
+  end_ip_address = "0.0.0.0"
+}
+
+resource "azurerm_service_plan" "sp" {
+  name                = "${var.app_service_plan_name}-${random_integer.ri.result}"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  os_type             = "Linux"
+  sku_name            = "F1"
+}
+
+resource "azurerm_linux_web_app" "webapp" {
+  name                = "${var.app_service_name}-${random_integer.ri.result}"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_service_plan.sp.location
+  service_plan_id     = azurerm_service_plan.sp.id
+
+  connection_string {
+    name = "DefaultConnection"
+    type = "SQLAzure"
+    value = "Data Source=tcp:${azurerm_mssql_server.sqlserver.fully_qualified_domain_name},1433;Initial Catalog=${azurerm_mssql_database.db.name};User ID=${azurerm_mssql_server.sqlserver.administrator_login};Password=${azurerm_mssql_server.sqlserver.administrator_login_password};Trusted_Connection=False; MultipleActiveResultSets=True;"
+  }
+
+  site_config {
+    application_stack {
+      dotnet_version = "8.0"
+    }
+    always_on = false
+  }
+}
+
+resource "azurerm_app_service_source_control" "sourcecontrol" {
+  app_id                 = azurerm_linux_web_app.webapp.id
+  repo_url               = var.repo_URL
+  branch                 = "master"
+  use_manual_integration = false
+}
